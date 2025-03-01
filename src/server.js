03e9b5bace7e06febc5c2d2635a39cc8cb48b2b6
MCVdev <mateocarvajalvergara333@gmail.com>
2025-02-28 19:05:54 -0500

    prisma error try to fix

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import pkg from '@prisma/client';
 import app from "./app.js";
 import {env} from "./config/env.js";
 
-const prisma = new PrismaClient();
+const { PrismaClient } = pkg;
 
 app.listen(env.port, () => {
   console.log(`the server is running on port: ${env.port}`);
